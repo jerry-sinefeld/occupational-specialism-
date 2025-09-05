f2463b6b459a175831aa972636aced3f195e2244
jerry-sinefeld <gamingtimemachine@gmail.com>
2025-09-05 14:32:56 +0100

    added PHP guides

--- May_26/html basics/ver 1/index.php
+++ May_26/html basics/ver 1/index.php
@@ -18,7 +18,7 @@ echo "<html>"; // opens html
 
         echo "has no hair";
 
-        echo "<a href = 'https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjb7MCvysGPAxWHUkEAHVshASsQFnoECDcQAQ&url=https%3A%2F%2Fwww.nhs.uk%2Fsymptoms%2Fhair-loss%2F&usg=AOvVaw1F4xFsgDLrgENaKiKx8hbl&opi=89978449'>  Go to page 2.</a>";
+        echo "<a href = 'https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjb7MCvysGPAxWHUkEAHVshASsQFnoECDcQAQ&url=https%3A%2F%2Fwww.nhs.uk%2Fsymptoms%2Fhair-loss%2F&usg=AOvVaw1F4xFsgDLrgENaKiKx8hbl&opi=89978449'>  Go to advice.</a>";
 
     echo "<img src='receding_hairline_test.jfif'>";
 
@@ -34,6 +34,21 @@ echo "<html>"; // opens html
             echo "<td>Hello 6 </td>";
             echo "</tr>";
 
+    echo "</table>";
+
+    echo "<ul>";
+    echo "<li>this is a bald man</li>";
+    echo "<li>he loves baby oil</li>";
+    echo "<li>so he can slip into nooks and crannies</li>";
+    echo "</ul>";
+
+    echo "<ol>";
+        echo "<li>he uses a razor</li>";
+        echo "<li>and a blowtorch</li>";
+        echo "<li>to keep his head nice and shiny</li>";
+    echo "</ol>";
+
+    echo "<a href='page 2.php'> go to page 2</a>";
 
     echo "</body>"; // closes body
 
---
+++ May_26/html basics/ver 1/page 2.php
@@ -0,0 +1,12 @@
+<?php
+
+    echo "<!DOCTYPE html>";
+
+echo "<html>"; // opens html
+    echo "<head>"; // opens head
+        echo "<title>Advice to reduce balding</title>"; // opens title,writes it and close it
+
+
+
+echo "</html>";
+?>
