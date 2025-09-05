e50d16686e0551f8f5315e90d87530debabaf0ab
jerry-sinefeld <gamingtimemachine@gmail.com>
2025-09-05 10:58:03 +0100

    started a html basics folder for my exams in may 26

--- May_26/html basics/ver 1/index.php
+++ May_26/html basics/ver 1/index.php
@@ -1,14 +1,19 @@
 <?php //this is a comment
 
+echo "<!DOCTYPE html>"; // desired tag for correct structure of an html page to declare what type it is
+
 echo "<html>"; // opens html
-echo "<head>"; // opens head
-echo "<title>May 26</title>"; // opens title,writes it and close it
+    echo "<head>"; // opens head
+        echo "<title>May 26</title>"; // opens title,writes it and close it
 
-echo "</head>"; // closes head
-echo "<body>"; // opens body
+    echo "</head>"; // closes head
+    echo "<body>"; // opens body
 
-echo "<h1>cody is balding</h1>"; // opens first header
+        echo "<h1>cody is balding</h1>"; // opens first header
 
-echo "this is my page."; // writes first paragraph
+        echo "this is my page."; // writes first paragraph
 
-echo "</body>"; // closes body
+    echo "</body>"; // closes body
+
+echo "</html>";
+?>
