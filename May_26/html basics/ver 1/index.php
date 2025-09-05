9e95eb7cf8c6e0f202eadef9c4c65a71725e5fff
jerry-sinefeld <gamingtimemachine@gmail.com>
2025-09-05 10:43:25 +0100

    started a html basics folder for my exams in may 26

---
+++ May_26/html basics/ver 1/index.php
@@ -0,0 +1,14 @@
+<?php //this is a comment
+
+echo "<html>"; // opens html
+echo "<head>"; // opens head
+echo "<title>May 26</title>"; // opens title,writes it and close it
+
+echo "</head>"; // closes head
+echo "<body>"; // opens body
+
+echo "<h1>cody is balding</h1>"; // opens first header
+
+echo "this is my page."; // writes first paragraph
+
+echo "</body>"; // closes body
