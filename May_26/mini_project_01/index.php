025593198d07144a86e6e6e1c4283110bd16c75b
jerry-sinefeld <gamingtimemachine@gmail.com>
2025-09-09 13:09:46 +0100

    made more additions aswell as links to pages

--- May_26/mini_project_01/index.php
+++ May_26/mini_project_01/index.php
@@ -13,6 +13,15 @@ echo "<html>"; //opens html
 
         echo "<h1>Welcome to the family, son!</h1><br>";
 
+        echo "<p>Are you a returning resident evil 7 player, or a fresh face taking their first steps into the baker estate?</p>";
+        echo "<p>no matter your answer you've come to the right place, here you will find all the neat tips and tricks that you'll need to take on the baker family!</p><br>";
+        echo "<p>First things first, you'll need to know what you're getting yourself into before you decide to begin. The resident evil games are known as survival horror games for a reason. You must survive!";
+        echo "<p>But what does that mean?! I hear you ask. Well it means that you are not doomguy or kratos, you're just a man and a gun with very limited ammunition!</p>";
+        echo "<p>Therefore,treat every bullet like it's your last.";
+        echo "<p>Now that we have the basics out of the way let's get down to specifics.";
+        echo "<p>Below are the components to your run. First is the <a href='bestiary.php'> bestiary</a>, this is where you'll find all the info you need on all the baddies you'll face during your residential visit.";
+        echo "<p>Then there's the <a href='encyclopedia.php'>encyclopedia</a> which will give you a complete rundown of every item in the game and it's use.";
+
 
 
 
