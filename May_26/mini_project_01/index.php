d32f1b5e1a8f0622bfd84b5387acab38e1fd9caf
jerry-sinefeld <gamingtimemachine@gmail.com>
2025-09-09 11:19:33 +0100

    added PHP guides

---
+++ May_26/mini_project_01/index.php
@@ -0,0 +1,25 @@
+<?php //OPENS PHP
+
+echo "!DOCTYPE html"; //declares the doc as a html so it follows the correct structure
+
+echo "<html>"; //opens html
+    echo "<head>"; //opens head
+        echo "<title>Resident evil completionist guide</title>"; //opens and writes title
+        echo "<link rel='stylesheet' href='CSS/styles.css'>";
+    echo "</head>";
+
+    echo "<body>";
+        echo "<p>RESIDENT E<span class='seven_color'>VIL</span> completionist guide</p>";
+
+
+
+
+
+
+
+
+
+
+
+echo "</html>"; //closes html
+?>
