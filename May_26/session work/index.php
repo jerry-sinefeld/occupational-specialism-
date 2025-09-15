ea26824c6f409c596520f09c78d37a432ced42ef
jerry-sinefeld <gamingtimemachine@gmail.com>
2025-09-15 14:17:08 +0100

    SESSION start stuff

--- May_26/session work/index.php
+++ May_26/session work/index.php
@@ -1,11 +1,16 @@
 <?php //OPENS php
 session_start();
 
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+}
+
+
 echo "<!DOCTYPE html>"; //declares the doc as a html so it follows the correct structure
 
 echo "<html>"; //opens html
     echo "<head>"; //opens head
-        echo "<title></title>"; //opens and writes title
+        echo "<title>Session work</title>"; //opens and writes title
         echo "<link rel='stylesheet' href='css/styles.css'>"; //links the file to the stylesheet which contains all the css
     echo "</head>"; //closes head
 
@@ -14,9 +19,10 @@ echo "<html>"; //opens html
     require_once "assets/nav.php";
     require_once "assets/topbar.php";
     echo "<div id='main'>";
-
-
-
+    echo "<h2>session work</h2>";
+    echo "<form method='post' action=''>";
+        echo "<input type='text' name='message' placeholder='message'>";
+        echo "<input type='submit' name='submit' value='submit'>"; //takes user input and store it in session to be outputted somewhere else
     echo "</div>";
     echo "</div>";
     echo "</body>";
