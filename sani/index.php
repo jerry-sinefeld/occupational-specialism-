73ea54296a5548d31949a12c3799b0e73932fcc5
jerry-sinefeld <gamingtimemachine@gmail.com>
2025-09-16 14:49:00 +0100

    Started work on sanitization

--- sani/index.php
+++ sani/index.php
@@ -14,30 +14,32 @@ echo "<html>"; //opens html
     require_once "assets/nav.php";
     require_once "assets/topbar.php";
     echo "<div id='main'>";
-    if ($_SERVER["REQUEST_METHOD"] == "POST") /*requests the server to post the inputted*/ {
-        echo "your string:" . $_POST["str"]; //posts name
-        echo "<br>"; //breakline
-        echo "your email:" . $_POST["email"]; //posts code
-        echo "<br>"; //breakline
-        echo "your url:" . $_POST["url"]; //posts password
-        echo "<br>"; //breakline
-        echo "number:" . $_POST["int"]; //posts the checkbox
-    }
-    echo "<form action='' method='post'>"; //creates the form and tells the form what to do
-    echo "<label for='str'>Name:</label>"; //creates the name text box and what it contains
-    echo "<input type='text' name='str' id='str' placeholder= 'enter your name' required>"; //creates the name field
-    echo $sanitized_string = filter_var($_POST["str"], FILTER_SANITIZE_STRING);
-    echo "<label for='email'>Email:</label>";
+
+    echo "<form method='post' action='' >"; //creates the form and tells the form what to do
+    #echo "<label for='str'>Name:</label>"; //creates the name text box and what it contains
+    echo "<input type='text' name='stri' placeholder= 'enter your name' >"; //creates the name field
+    #echo "<label for='email'>Email:</label>";
     echo "<input type='email' name='email' placeholder='Email'/>"; //creates the email field
-    echo $sanitized_email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-    echo "<label for='url'>URL:</label>";
+    #echo "<label for='url'>URL:</label>";
     echo "<input type='url' name='url' placeholder='URL'/>";
-    echo $sanitized_url = filter_var($_POST["url"], FILTER_SANITIZE_URL);
-    echo "<label for='int'>enter a number:</label>";
+    #echo "<label for='int'>enter a number:</label>";
     echo "<input type='number' name='int' placeholder='enter a number'/>";
-    echo $sanitized_int = filter_var($_POST["int"], FILTER_SANITIZE_NUMBER_INT);
     echo "<br>"; //breakline
     echo "<input type='submit' name='submit' value='Submit'>"; //creates the submit box
+    echo "</form>";
+if ($_SERVER["REQUEST_METHOD"] === "POST") /*requests the server to post the inputted*/ {
+    #echo "your string: " . $_POST['stri'];
+    echo "your string: " .filter_var($_POST['stri'], FILTER_SANITIZE_STRING);
+    echo "<br>"; //breakline
+    echo "your email:" . $_POST['email']; //posts code
+    #echo  filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    echo "<br>"; //breakline
+    echo "your url:" . $_POST['url']; //posts password
+    #echo  filter_var($_POST['url'], FILTER_SANITIZE_URL);
+    echo "<br>"; //breakline
+    echo "number:" . $_POST['int']; //posts the checkbox
+    #echo  filter_var($_POST['int'], FILTER_SANITIZE_NUMBER_INT);
+}
 
     echo "</div>";
     echo "</div>";
