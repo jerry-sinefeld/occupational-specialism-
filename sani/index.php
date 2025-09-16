4854b8678d275dde7c9a12b4a21d5e842e5d4f85
jerry-sinefeld <gamingtimemachine@gmail.com>
2025-09-16 12:42:10 +0100

    Started work on sanitization

--- sani/index.php
+++ sani/index.php
@@ -14,19 +14,27 @@ echo "<html>"; //opens html
     require_once "assets/nav.php";
     require_once "assets/topbar.php";
     echo "<div id='main'>";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") /*requests the server to post the inputted*/ {
+        echo "your string:" . $_POST["str"]; //posts name
+        echo "<br>"; //breakline
+        echo "your email:" . $_POST["email"]; //posts code
+        echo "<br>"; //breakline
+        echo "your url:" . $_POST["url"]; //posts password
+        echo "<br>"; //breakline
+        echo "number:" . $_POST["int"]; //posts the checkbox
+    }
     echo "<form action='' method='post'>"; //creates the form and tells the form what to do
     echo "<label for='str'>Name:</label>"; //creates the name text box and what it contains
     echo "<input type='text' name='str' id='str' placeholder= 'enter your name' required>"; //creates the name field
+    echo "$sanitized_string = filter_var(str, FILTER_SANITIZE_STRING)";
     echo "<label for='email'>Email:</label>";
     echo "<input type='email' name='email' placeholder='Email'/>"; //creates the email field
     echo "<label for='url'>URL:</label>";
     echo "<input type='url' name='url' placeholder='URL'/>";
     echo "<label for='int'>enter a number:</label>";
     echo "<input type='number' name='int' placeholder='enter a number'/>";
-
-
-
-
+    echo "<br>"; //breakline
+    echo "<input type='submit' name='submit' value='Submit'>"; //creates the submit box
 
     echo "</div>";
     echo "</div>";
