e58ba4aaa9b65a1adcbdcc9c4b1e56260ff3523a
jerry-sinefeld <gamingtimemachine@gmail.com>
2025-09-21 20:57:15 +0100

    Completely sweated out comments

--- May_26/mini_project_03/Solution/ver_1/index.php
+++ May_26/mini_project_03/Solution/ver_1/index.php
@@ -10,12 +10,12 @@ echo "<html>"; //opens html
     echo "</head>"; //closes head
 
     echo "<body>"; //opens body
-    echo "<div class='container'>";
-    require_once "assets/topbar.php";
-    require_once "assets/nav.php";
-    echo "<div id='main'>";
+    echo "<div class='container'>"; //opens the div with the class of main
+    require_once "assets/topbar.php"; //opens top bar file
+    require_once "assets/nav.php"; //opens assets file
+    echo "<div id='main'>"; //opens the div with the id of main
         echo "<p>A strong password is your first line of defense against cyber threats, but many people still use simple, easy-to-guess combinations. Creating a secure password isn't just about making it difficult for others to figure out; it's about making it computationally difficult for a machine to crack. Your password should have at least 8 characters and use a diverse set of characters. It must include at least one uppercase letter, at least one lowercase letter, at least one number, and at least one special character. The first character cannot be a special character or a number, and the last character cannot be a special character. Your password cannot contain the word 'password'. By following these guidelines, you can significantly reduce your risk of becoming a victim of a cyberattack.<p/>";
-    echo "</div>";
-    echo "</div>";
-    echo "</body>";
+    echo "</div>"; //closes main div
+    echo "</div>"; //closes container div
+    echo "</body>"; //closes body
 echo "</html>"; //closes html
--- May_26/mini_project_03/Solution/ver_1/pass_checker.php
+++ May_26/mini_project_03/Solution/ver_1/pass_checker.php
@@ -24,56 +24,57 @@ echo "<html>"; //opens html
                 if ($_SERVER["REQUEST_METHOD"] == "POST") /*requests the server to post the inputted*/ {
                     $_tally = 9; //creates a variable with a value of 9 as that is how many requirements there are
                     $_password = $_POST['pass']; //defines the variable of $_password as whatever is inputted into the pass input
-                    $_spec_char = '/[!@#$"£%^&*()_=+{};:,.<>]/'; //a variable with a collection of all the special characters I could think of, I'm sure there's a better way
+                    $_spec_char = '/[!@#$"£%^&*()_=+{};:,.<>]/'; //a variable with a collection of all the special characters I could think of, I'm sure there's a better way of doing this.
 
                     if (!preg_match('/[A-Z]/', $_password)) { //checks if an uppercase character doesn't exist in the password variable
                         $_tally --; //takes one off the tally if it doesn't
                         echo "your password must contain at least one uppercase character"; //improvement message
-                        echo "<br>";
+                        echo "<br>"; //breakline
                     }
-                    if (!preg_match('/[a-z]/', $_password)) {
-                        $_tally --;
+                    if (!preg_match('/[a-z]/', $_password)) { //checks if a lowercase character doesn't exist in the password variable
+                        $_tally --; //takes one off the tally if it doesn't
                         echo "your password must contain at least one lowercase character"; //improvement message
-                        echo "<br>";
+                        echo "<br>"; //breakline
                     }
-                    if (!preg_match('/[0-9]/', $_password)) {
-                        $_tally --;
+                    if (!preg_match('/[0-9]/', $_password)) { //checks if the password doesn't contain a number
+                        $_tally --; //takes one off the tally if it doesn't
                         echo "your password must contain at least one number"; //improvement message
-                        echo "<br>";
+                        echo "<br>"; //breakline
                     }
-                    if (strlen($_password) <= 8) {
-                        $_tally --;
+                    if (strlen($_password) <= 8) { //checks if the strings length is less than or equal to 8
+                        $_tally --; //takes one off the tally if it is
                         echo "Your password must be at least 8 characters long."; //improvement message
-                        echo "<br>";
+                        echo "<br>"; //breakline
                     }
-                    if (preg_match('/^[0-9]/', $_password)) {
-                        $_tally --;
+                    if (preg_match('/^[0-9]/', $_password)) { //checks to see if there is a number at the start of the password using ^
+                        $_tally --; //takes one off the tally if there is
                         echo "Your password should not start with a number."; //improvement message
-                        echo "<br>";
+                        echo "<br>"; //breakline
                     }
-                    if (preg_match($_spec_char, $_password[0])) {
-                        $_tally --;
+                    if (preg_match($_spec_char, $_password[0])) { //checks if there's a special character at the beginning of the string using the [0]
+                        $_tally --; //takes one off the tally if there is
                         echo "Your password should not start with a special character."; //improvement message
-                        echo "<br>";
+                        echo "<br>"; //breakline
                     }
-                    if (preg_match($_spec_char, substr($_password, -1))) {
-                        $_tally --;
+                    if (preg_match($_spec_char, substr($_password, -1))) { /*checks if the last character is a special character. It does this by using the substr() function which
+                        returns part of a given string (entered password) I got it to specifically return the last character in the string using the -1 offset which returns x amount at the end of the string */
+                        $_tally --; //takes one off the tally if there is
                         echo "Your password should not end with a special character."; //improvement message
-                        echo "<br>";
+                        echo "<br>"; //breakline
                     }
-                    if (!preg_match($_spec_char, $_password)) {
-                        $_tally --;
+                    if (!preg_match($_spec_char, $_password)) { //checks if password doesn't contain a special character
+                        $_tally --; //takes one off the tally if it doesn't
                         echo "Your password must contain at least one special character."; //improvement message
-                        echo "<br>";
+                        echo "<br>"; //breakline
                     }
-                    if ($_password === 'password') {
-                        $_tally --;
+                    if ($_password === 'password') /* used an absolute comparator to check if password = password*/ {
+                        $_tally --; //takes one off the tally if password = password
                         echo "Your password cannot be password or Password."; //improvement message
-                        echo "<br>";
+                        echo "<br>"; //breakline
                     }
-                    echo "your password is: " . $_POST['pass'];
-                    echo "<br>";
-                    echo "your score is: " . $_tally . "/9";
+                    echo "your password is: " . $_POST['pass']; //prints out the inputted password
+                    echo "<br>"; //breakline
+                    echo "your score is: " . $_tally . "/9"; //prints out your score is and then the tally variable which is where score is stored
                 }
             echo "</div>"; //closes main div
         echo "</div>"; //closes container div
