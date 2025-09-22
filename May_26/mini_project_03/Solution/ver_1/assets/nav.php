0d4ff41956bb1fd2edf9ee12bd9243e32d7038ff
jerry-sinefeld <gamingtimemachine@gmail.com>
2025-09-22 09:41:30 +0100

    finished mini project 3

--- May_26/mini_project_03/Solution/ver_1/assets/nav.php
+++ May_26/mini_project_03/Solution/ver_1/assets/nav.php
@@ -4,7 +4,7 @@ echo "<div class='navi'/>";
     echo "<nav>";
         echo "<ul>";
             echo "<li> <a href='index.php'>Home</a></li>";
-            echo "<li> <a href='pass_checker'>Password checker</a></li>";
+            echo "<li> <a href='pass_checker.php'>Password checker</a></li>";
         echo "</ul>";
     echo "</nav>";
 
