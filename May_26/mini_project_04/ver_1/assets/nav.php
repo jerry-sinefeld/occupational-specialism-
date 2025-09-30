140dcd8c3d1f71ab7302007dc362e30d237c355b
jerry-sinefeld <gamingtimemachine@gmail.com>
2025-09-30 12:24:22 +0100

    started the mini project 4

---
+++ May_26/mini_project_04/ver_1/assets/nav.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "<div class='navi'/>";
+    echo "<nav>";
+        echo "<ul>";
+            echo "<li> <a href='link_1'>link1</a></li>";
+            echo "<li> <a href='link_2'>link2</a></li>";
+            echo "<li> <a href='link_3'>link3</a></li>";
+            echo "<li> <a href='link_4'>link4</a></li>";
+        echo "</ul>";
+    echo "</nav>";
+
+echo "</div>";
